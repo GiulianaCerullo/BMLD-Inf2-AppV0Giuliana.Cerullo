95c69fb4e47b9f2c183d8653bd942c9eba6e1d62
Ladina <ladina.lozza@bluewin.ch>
2026-05-13 09:12:26 +0200

    login usw.

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Bakterienidentifikation", page_icon="🦠")
-
-pg_home = st.Page("views/home.py", title="Home")
-pg_gram_pos = st.Page("views/Gram_positiv.py", title="Gram-positiv", default=True)
-pg_gram_neg = st.Page("views/Gram_negativ.py", title="Gram-negativ")
-pg_tests = st.Page("views/Testbeschreibung.py", title="Testbeschreibungen")
-pg_lernen = st.Page("views/Lernen.py", title="Lernen")
-pg_steckbriefe = st.Page("views/Steckbrief.py", title="Steckbriefe")
-
-pg = st.navigation([pg_home, pg_gram_pos, pg_gram_neg, pg_tests, pg_lernen, pg_steckbriefe])
-pg.run()
-
-import pandas as pd  # --- NEW CODE: add pandas to the imports ---
-import streamlit as st
-
 # --- NEW CODE: import and initialize data manager and login manager ---
 from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
@@ -36,3 +21,13 @@ if 'data_df' not in st.session_state:
         parse_dates=['timestamp']       # Parse timestamp as datetime
     )
 # --- END OF CODE UPDATE ---
+
+pg_home = st.Page("views/home.py", title="Home")
+pg_gram_pos = st.Page("views/Gram_positiv.py", title="Gram-positiv", default=True)
+pg_gram_neg = st.Page("views/Gram_negativ.py", title="Gram-negativ")
+pg_tests = st.Page("views/Testbeschreibung.py", title="Testbeschreibungen")
+pg_lernen = st.Page("views/Lernen.py", title="Lernen")
+pg_steckbriefe = st.Page("views/Steckbrief.py", title="Steckbriefe")
+
+pg = st.navigation([pg_home, pg_gram_pos, pg_gram_neg, pg_tests, pg_lernen, pg_steckbriefe])
+pg.run()
