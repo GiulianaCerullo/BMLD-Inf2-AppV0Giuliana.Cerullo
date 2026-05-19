a9fc0d1032f80d2f95117c0defb4cf6477fd21f8
Giuliana <cerulgiu@students.zhaw.ch>
2026-05-19 09:19:00 +0200

    Steckbriefe

--- views/Steckbrief.py
+++ views/Steckbrief.py
@@ -68,11 +68,93 @@ st.markdown("""
 st.markdown("## Grampositiv")
 with st.expander("**Listerien**"):
     st.markdown("""
-**Steckbrief:**
 - Stäbchenförmige Bakterien
 - Wichtigste krankmachende Art: Listeria monocytogenes
 - Beweglich durch Geisseln
 - Vermehren sich auch bei Kühlschranktemperaturen (ca. 4 °C)
 - Sehr widerstandsfähig gegen Umweltbedingungen 
 - Listerien können die Krankheit Listeriose auslösen.
-""")                                                                         
+""")
+
+with st.expander("**Bacillus**"):
+    st.markdown("""
+- Stäbchenförmige Bakterien
+- Können Sporen bilden
+- Viele Arten beweglich durch Geisseln
+- Meist aerob 
+- Wichtige Arten:
+   - Bacillus Subtilis: Meist harmlos, wird in Forschung genutzt
+   - Bacillus cereus: Kann Lebensmittelvergiftungen verursachen                                                                                         
+   - Bacillus anthracis: Erreger von Milzbrand
+""")
+
+with st.expander("**Corynebakterien**"):
+    st.markdown("""
+- Keulenförmige Stäbchenbakterien
+- Oft unregelmäßige Anordnung („V-“ oder „Y-Form“)
+- Unbeweglich
+- Bilden keine Sporen
+- Aerob
+- Bilden schwarze Kolonien               
+- Einige Arten sind harmlos und gehören zur normalen Hautflora
+- Wichtige Arten: 
+    - Corynebacterium diphtheriae → verursacht Diphtherie
+    - Corynebacterium jeikeium → kann Krankenhausinfektionen auslösen  
+""")
+
+with st.expander("**Clostridien**"):
+    st.markdown("""
+- Stäbchenförmige Bakterien
+- Viele Arten leben im Boden oder im Darm von Mensch und Tier
+- Bilden widerstandsfähige Sporen
+- Meist aerob
+- Vorbeugung durch gute Hygiene und Tetanus Impfung                
+- Wichtige Arten:
+    - Clostridium tetani: Verursacht Wundstarrkrampf (Tetanus)
+    - Clostridium botulinum: Bildet das Botulinumtoxin                                                                                                               
+    - Clostridum difficile: Kann schwere Darminfektionen verursachen
+    - Clostridium perfringens: Verursacht Lebensmittelvergiftungen
+""")
+
+with st.expander("**Cutibakterien**"):
+    st.markdown("""
+- Stäbchenförmige Bakterien
+- Früherer Name: Propionibacterium
+- Gehören zur normalen Hautflora
+- Wachsen bevorzugt ohne Sauerstoff (anaerob)
+- Wichtige Arten
+    - Cutibacterium acnes → beteiligt an Akn  
+""")
+
+with st.expander("**Staphylococcus aureus**"):
+    st.markdown("""
+- Kugelförmig (Kokken), traubenförmig angeordnet
+- Katalase positiv
+- Koagulase positiv
+- Betha- Hämolyse
+- Widerstandsfähig gegen Umwelteinflüsse
+- Bildet oft gelbliche Kolonien („aureus“ = golden)
+- Manche Stämme sind antibiotikaresistent
+- Wichtige Form:
+    - MRSA = Methicillin-resistenter Staphylococcus aureus
+    → resistent gegen viele Antibiotika
+""")
+
+with st.expander("**Staphylococcus epidermidis**"):
+    st.markdown("""
+- Kugelförmiges Bakterium (Kokken)
+- Katalase positiv
+- Koagulase negativ
+- keine Hämolyse                                               
+- Teil der normalen Hautflora des Menschen                
+- Novobiocinsensitiv
+""")
+
+with st.expander("**Staphylococcus saprophyticus**"):
+    st.markdown("""
+- Kugelförmig (Kokken), traubenförmig angeordnet
+- Fakultativ pathogen (meist harmlos, kann aber Krankheiten auslösen)
+- Katalase positiv
+- Koagulase negativ
+- Novobiocin resistent 
+""")                                                       
